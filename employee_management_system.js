42fccbe37c1828beccbb8d29e171c5ebca3091d5
Kosi Mensah <kmensah@usf.edu>
2024-10-05 17:00:31 -0400

    Create Department class to manage employees and calculate salary

--- employee_management_system.js
+++ employee_management_system.js
@@ -2,8 +2,8 @@
 class Employee {
     constructor(name, salary, position, department) { // making a class of employee properties
         this.name = name;  
-        this.salary = salary;   
-        this.position = position; 
+        this.position = position;   
+        this.salary = salary; 
         this.department = department; 
     }
 
@@ -11,3 +11,16 @@ class Employee {
         return(`${this.name} works as a ${this.position} earning a Salary of: $${this.salary} annually`); // Method to get employee details as an organized string
     }
 }
+
+class Department{ //creates Department class
+    constructor(name, employees){
+        this.name = name; // name of department
+        this.employees = [];  //empty employee array
+    }
+
+    addEmployee(employee){
+        this.employees.push(employee); //Method to add employee to array
+    }
+    getDepartmentSalary(){
+        return this.employees.reduce((total, employee) => total + employee.salary, 0); //Calculates the total department salary
+    }}
